240c7dfe82e822860926c0bc710302cd7e6d8f44
Dani <ifcd0210.dani@gmail.com>
2025-06-02 19:52:56 +0200

    Add carousel to #historias and fix curve divider visibility

--- js/script.js
+++ js/script.js
@@ -60,3 +60,27 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('scrolled');
   }
 });
+
+// Inicializar Swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
