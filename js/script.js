0c2475234a3f696b86f685fb1f46345dd3ee6c6b
Dani <ifcd0210.dani@gmail.com>
2025-06-03 18:44:15 +0200

    Fix carousel to show all stories in Explora nuestras historias

--- js/script.js
+++ js/script.js
@@ -63,23 +63,27 @@ window.addEventListener('scroll', function() {
 
 // Inicializar Swiper
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 'auto',
+  slidesPerView: 4, // Mostrar 4 slides por defecto
   spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-    640: {
+    320: {
       slidesPerView: 1,
       spaceBetween: 15,
     },
-    768: {
+    640: {
       slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 3,
       spaceBetween: 20,
     },
     1024: {
-      slidesPerView: 3,
+      slidesPerView: 4,
       spaceBetween: 20,
     },
   },
